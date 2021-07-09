5efdb13e2de65ea8d176bd787fca49f5ea3b5e4e
Nestor Alejandro Mendez <you@example.com>
2021-07-08 18:28:02 -0600

    Mongo atlas y Heroku

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var port = 3800;
 
 mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost:27017/DB_SportTournament', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://admin:admin@cluster0.9qvg3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
         user.initAdmin();
         teamInit.teamdefault();
         console.log('Conexion correcta a la base de datos');
-        app.listen(port, ()=>{
+        app.listen(process.env.PORT || port, ()=>{
             console.log('EL servidor esta corriendo en el puerto' + port)
         })
     })
